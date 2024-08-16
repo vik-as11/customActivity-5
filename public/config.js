72674ffebbf22e4c0cbb5192ad046aa8c293b012
vik-as11 <58174145+vik-as11@users.noreply.github.com>
2024-08-16 15:21:12 +0530

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,22 +1,19 @@
-// config.mjs or config.js
-export function configJSON(req) {
-  return {
-    name: "My Custom Activity",
-    description: "A description of the custom activity",
-    icon: "path/to/icon.png",
-    edit: {
-      url: "https://your-edit-url.com"
+window.config = {
+    "name": "My Custom Activity",
+    "description": "A description of the custom activity",
+    "icon": "path/to/icon.png",
+    "edit": {
+        "url": "https://your-edit-url.com"
     },
-    execute: {
-      url: "https://your-execute-url.com"
+    "execute": {
+        "url": "https://your-execute-url.com"
     },
-    schema: {
-      arguments: {
-        execute: {
-          inArguments: [],
-          outArguments: []
+    "schema": {
+        "arguments": {
+            "execute": {
+                "inArguments": [],
+                "outArguments": []
+            }
         }
-      }
     }
-  };
-}
+};
