833cfd751de1605721c53297b1ab52e709a4e1a7
vik-as11 <58174145+vik-as11@users.noreply.github.com>
2024-08-16 13:44:26 +0530

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "name": "My Custom Activity",
+    "description": "A description of the custom activity",
+    "icon": "path/to/icon.png",
+    "edit": {
+        "url": "https://your-edit-url.com"
+    },
+    "execute": {
+        "url": "https://your-execute-url.com"
+    },
+    "schema": {
+        "arguments": {
+            "execute": {
+                "inArguments": [],
+                "outArguments": []
+            }
+        }
+    }
+};
