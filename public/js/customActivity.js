433cfaa8d8558cc13ce661352f0e7b99cfb08a8b
vik-as11 <58174145+vik-as11@users.noreply.github.com>
2024-08-20 13:17:22 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -115,7 +115,8 @@ define([
         fetch('https://api.talkdeskappca.com/digital-connect/conversations', {  // Replace with your actual API endpoint
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization' : 'Bearer eyJraWQiOiJmOWE5NDg2MGU5YmFlNTQxYTViYWM2NTAzM2Q0NTc4NSIsImFsZyI6IkVTMjU2In0.eyJpc3MiOiJodHRwczovL3d3dy50YWxrZGVza2lkY2EuY29tIiwiYXVkIjoiaHR0cHM6Ly9hcGkudGFsa2Rlc2thcHBjYS5jb20iLCJleHAiOjE3MjQxNDA0OTcsIm5iZiI6MTcyNDEzOTg5NywiaWF0IjoxNzI0MTM5ODk3LCJqdGkiOiI2NmU5YmVmYjljYWQ0NzkwOTVkYTViYmEzODZkNDA4ZCIsImNpZCI6IjM3MWVhMTJjYzkwMTRmOWI4OGM3ODE0YjA5ZDIwMDMwIiwiZ3R5IjoiY2xpZW50X2NyZWRlbnRpYWxzIiwic2NwIjpbImFjY291bnQ6cmVhZCIsImFwcHM6d3JpdGUiLCJjb250YWN0czpyZWFkIiwiZGlnaXRhbC1jb25uZWN0OndyaXRlIiwib3BlbmlkIiwicmVjb3JkLWxpc3RzOm1hbmFnZSJdLCJybG0iOiJNQUlOIiwiYWlkIjoiNjJiZTNlY2YxM2M3NTY0Zjc4NjBjMmIwIiwiYWNjIjoibnVvdm8iLCJwc24iOiJDVVNUT01FUiIsInJlZyI6ImNhLWNlbnRyYWwtMSIsInN1YiI6IjM3MWVhMTJjYzkwMTRmOWI4OGM3ODE0YjA5ZDIwMDMwIn0.SRvZHL18Xu_zT4yrgBhQD0xdOCRoWZfm3mWK33naSNe5w6UC5U989lLVvXQzAiemVdZzhf8IpzleMr87qT4JOQ'
             },
             body: JSON.stringify(apiRequestBody)
         })
